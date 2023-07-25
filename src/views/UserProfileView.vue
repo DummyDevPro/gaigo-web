1cead0493b27074f9b0481b3a856f1f1b36031e9
DummyDevPro <htun@osf.ac.jp>
2023-07-26 00:03:41 +0900

    add filter to filter answers data table not fine code need modifie

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -16,6 +16,22 @@
         <section class="exam-result-layout px-3">
             <h4>Your Exam Results</h4>
             <div class="ps-3 pe-3">
+
+                <div v-if="filterAnswersData.length != 0"
+                    class="table-controller d-flex align-items-center gap-2 justify-content-end">
+                    <span>
+                        Showing <span>{{ currentFilterPageStartNumber }}</span>
+                        to {{ currentFilterPage * filterLimit > totalAnswersDataCount ? totalAnswersDataCount :
+                            currentFilterPage * filterLimit }}
+                        of {{ totalAnswersDataCount }}
+                    </span>
+                    <i @click="updateFilterAnswersData('minus')" :class="currentFilterPage == 1 ? 'd-none' : 'd-block'"
+                        class="bi bi-caret-left left-icon"></i>
+                    <i @click="updateFilterAnswersData('plus')"
+                        :class="currentFilterPage == filterCount ? 'd-none' : 'd-block'"
+                        class="bi bi-caret-right right-icon"></i>
+                </div>
+
                 <table class="table caption-top table-hover">
                     <thead>
                         <tr>
@@ -27,10 +43,11 @@
                         </tr>
                     </thead>
                     <tbody class="align-middle">
-                        <tr v-if="answersData.length == 0">
+                        <tr v-if="filterAnswersData.length == 0">
                             <td colspan="100%" class="text-center">There is no exam records.</td>
                         </tr>
-                        <tr @click="moveToExamResultDetailsPage(ans.docId)" v-for="ans in answersData"
+
+                        <tr @click="moveToExamResultDetailsPage(ans.docId)" v-for="ans in filterAnswersData"
                             title="Click On Show Details" class="align-middle">
                             <td>
                                 {{ convertTimeStampToDate(ans?.['uploaded-time']) }} <br>
@@ -53,10 +70,32 @@ export default {
     data() {
         return {
             'answersData': [],
-            'personIcon': personIcon
+            'filterAnswersData': [],
+            'personIcon': personIcon,
+            'totalAnswersDataCount': 0,
+            'filterLimit': 5,
+            'filterCount': 0,
+            'currentFilterPageStartNumber': 1,
+            'currentFilterPage': 1,
         }
     },
     methods: {
+        updateFilterAnswersData(key) {
+            if (key == 'plus') {
+                this.currentFilterPage++
+            } else {
+                this.currentFilterPage--
+            }
+            const start = (this.currentFilterPage - 1) * this.filterLimit
+            const stop = this.currentFilterPage * this.filterLimit
+            this.currentFilterPageStartNumber = start + 1
+            this.filterAnswersData = []
+            for (let i = start; i < stop; i++) {
+                if (this.answersData[i] && this.answersData[i] != undefined) {
+                    this.filterAnswersData.push(this.answersData[i])
+                }
+            }
+        },
         moveToExamResultDetailsPage(uid) {
             this.$router.push({
                 name: 'exam-result-by-id', params: {
@@ -98,6 +137,18 @@ export default {
             (_, getters) => getters.acquireAnswersData('all'),
             (newValue, _) => {
                 this.answersData = newValue
+                this.totalAnswersDataCount = this.answersData.length
+                if (this.totalAnswersDataCount > 0) {
+                    this.filterCount = Math.ceil(this.totalAnswersDataCount / this.filterLimit)
+
+                    let start = this.currentFilterPage - 1
+                    let stop = this.currentFilterPage * this.filterLimit
+                    for (let i = start; i < stop; i++) {
+                        if (this.answersData[i] && this.answersData[i] != undefined) {
+                            this.filterAnswersData.push(this.answersData[i])
+                        }
+                    }
+                }
             }
         )
 
@@ -145,5 +196,22 @@ export default {
 
 .exam-result-layout table tbody tr {
     cursor: pointer;
+}
+
+.left-icon,
+.right-icon {
+    color: white;
+    height: fit-content;
+    width: fit-content;
+    background-color: var(--color-g-web);
+    padding: .25rem .5rem;
+    border-radius: 50%;
+    cursor: pointer;
+    transition: all .3s linear;
+}
+
+.left-icon:hover,
+.right-icon:hover {
+    box-shadow: 2px 0 4px black;
 }
 </style>
